f9e17f662e4cb3c9210d4da668bb4c53f1f1b809
jaquiel <jaquieljorge@hotmail.com>
2024-12-09 22:37:09 -0300

    stylizacao css

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,27 +5,48 @@ import Lixeira from '../../assets/lixeira.png'
 
 function Home() {
 
+  const users = [{
+    id: '2342asdfg',
+    name: 'jaquiel',
+    age: 37,
+    eamil: 'jaqueil.moreno@oultook.com',
+  },
+  {
+    id: '23423456asdfg',
+    name: 'heloisa',
+    age: 37,
+    eamil: 'holoisas.moreno@oultook.com',
+  },
+  ]
+
 
   return (
 
     <div className='container'>
-      <form><h1>Cadastro de Usuários</h1></form>
-      <input name='name' type='text' />
-      <input name='age' type='number' />
-      <input name='email' type='text' />
+      <form>
+        <h1>Cadastro de Usuários</h1>
+        <input placeholder='Name' name='name' type='text' />
+        <input placeholder='Age' name='age' type='number' />
+        <input placeholder='Email' name='email' type='text' />
+        <button type='button'>Cadastrar</button>
+      </form>
 
-      <button type='button'>Cadastrar Usuário</button>
+      {users.map((user) => (
 
-      <div>
-        <div>
-            <p>Name:  </p>
-            <p>Age:   </p>
-            <p>Email: </p>
+        <div key={user.id} className='card'>
+          <div>
+            <p>Name:   {user.name} </p>
+            <p>Age:    {user.age} </p>
+            <p>Email:  {user.eamil}</p>
+            </div>
             <button>
-                <img src={Lixeira} />
+              <img src={Lixeira} />
             </button>
+          
         </div>
-      </div>
+
+      ))}
+
 
 
 
