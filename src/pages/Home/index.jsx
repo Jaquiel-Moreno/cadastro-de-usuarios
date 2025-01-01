eb35beaf06b20d33235868a98d44fd89e5e8ee1d
jaquiel <jaquieljorge@hotmail.com>
2025-01-01 15:33:02 -0300

    Atulizacao campos adicionados ao projeto front

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -15,6 +15,8 @@ function Home() {
   const inputName = useRef()
   const inputAge = useRef()
   const inputEmail = useRef()
+  const inputUF  = useRef()
+ 
 
   async function getUsers() {
     const usersFropmApi = await api.get('/usuarios')
@@ -27,7 +29,9 @@ function Home() {
     await api.post('/usuarios', {
       name: inputName.current.value,
       age: inputAge.current.value,
-      email: inputEmail.current.value
+      email: inputEmail.current.value,
+      uf:  inputUF.current.value
+   
     })
     getUsers()
   }
@@ -36,6 +40,13 @@ function Home() {
     await api.delete (`usuarios/${id}`)
   
     getUsers()
+  }
+
+  function clearFields() {
+    inputName.current.value = '';
+    inputAge.current.value = '';
+    inputEmail.current.value = '';
+    inputUF.current.value = '';
   }
   
 
@@ -50,18 +61,23 @@ function Home() {
       <form>
         <h1>Cadastro de Usuários</h1>
         <input placeholder='Name' name='name' type='text' ref={inputName} />
-        <input placeholder='Age' name='age' type='number' ref={inputAge} />
+        <input placeholder='Age' name='age'type='number' ref={inputAge} />
         <input placeholder='Email' name='email' type='text' ref={inputEmail} />
+        <input placeholder='UF' name='uf' type='text' ref={inputUF} />
         <button type='button' onClick={createUsers}>Cadastrar</button>
+        <button className='clearField' type="button" onClick={clearFields}>Limpar Campos</button>
       </form>
+
+
 
       {users.map((user) => (
 
         <div key={user.id} className='card'>
           <div>
-            <p>Name:   <span>{user.name}</span> </p>
-            <p>Age:    <span>{user.age} </span></p>
-            <p>Email:  <span>{user.email}</span></p>
+            <p>Name:   <span>{user.name}  </span></p>
+            <p>Age:    <span>{user.age}   </span></p>
+            <p>Email:  <span>{user.email} </span></p>
+            <p>UF:     <span>{user.uf}    </span></p>
           </div >
           <button onClick={ () => deleteUsers(user.id)}>  
             <img src={Lixeira} title='delete' />
